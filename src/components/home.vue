71dfe95b2776fd73438e03d7ce4779c17d136e30
mjp <1685372168@qq.com>
2017-11-04 17:22:21 +0800

    完善

--- src/components/home.vue
+++ src/components/home.vue
@@ -9,9 +9,11 @@
             </el-col>   
             <el-col :span="5" class="userinfo">
                 <el-dropdown trigger="hover" @command="handleCommand">
-                    <span class="el-dropdown-link userinfo-inner"><img :src="this.sysUserAvatar" /> {{sysUserName}}</span>
+                <span class="el-dropdown-link userinfo-inner">{{sysUserName}}<el-badge is-dot :hidden="orderStatus"><img :src="this.sysUserAvatar" /></el-badge></span>
                     <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item>我的消息</el-dropdown-item>
+                        <el-badge :value="orderMsg" :max="10">
+                            <el-dropdown-item command="orderMsgs">我的消息</el-dropdown-item>
+                        </el-badge>
                         <el-dropdown-item>设置</el-dropdown-item>
                         <el-dropdown-item divided command="exit">退出登录</el-dropdown-item>
                     </el-dropdown-menu>
@@ -66,10 +68,15 @@
 </template>
 
 <script type="text/javascript">
+    import io from '../utils/socket.io.min.js';
+    var socket = io.connect('http://localhost:5544');
     export default {
         data() {
             return {
                 sysName:'RSOrder',
+                order:[],
+                orderStatus:true,
+                orderMsg:0,
                 sysUserName: 'Admin',
                 sysUserAvatar: './src/assets/img/a8.jpg',
                 isCollapse: false,
@@ -96,11 +103,38 @@
                     sessionStorage.removeItem('user');
                     this.$router.push({ path: '/login' });
 
+                }else if(command=='orderMsgs'){
+                    var _this = this;
+                    this.order.forEach(function(item,idx){
+                        var content = '';
+                        JSON.parse(item.params.orderContent).forEach(function(item,idx){
+                            content += item.count+'份'+item.menuName+" ";
+                        })
+
+                        _this.$notify({
+                            title: '成功下单',
+                            dangerouslyUseHTMLString: true,
+                            message: '订单号：'+ item.params.orderId + '<br>'+ content,
+                            type: 'success'
+                        });
+                    })
+                    this.orderStatus = true;
+                    this.orderMsg = 0;
+                    
                 }
             }
         },
         created:function(){
             this.limit = sessionStorage.getItem("user").slice(1,-1);
+        },
+        mounted: function(){
+         
+           socket.on('showMsg',(data)=>{
+                this.orderStatus = false;
+                var data = JSON.parse(data);
+                this.order.push(data);
+                this.orderMsg++;
+           });
         }
     }
 </script>
@@ -129,7 +163,7 @@
                     width: 40px;
                     height: 40px;
                     border-radius: 20px;
-                    margin: 10px 0px 10px 10px;
+                    margin-left:10px;
                     float: right;
                 }   
             }
