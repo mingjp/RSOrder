330e9d63ce9eeea2e6d396063b36695e105a4968
mjp <1685372168@qq.com>
2017-11-04 16:48:50 +0800

    更改

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
-<<<<<<< HEAD
         <datagrid api="user" modify="user_modify" delete="user_delete" add="user_add" pagelen="3"></datagrid>
-=======
-        <datagrid api="user" modify="user_modify" delete="user_delete" add="user_add"></datagrid>
->>>>>>> 20dd63a052964fa874c2d3281cd0bd3e42fccfa2
     </div>
 </template>
 
